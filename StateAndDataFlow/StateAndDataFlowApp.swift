9cd8e01d948b468e8ba1788fefb362c6b4b05399
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-13 21:50:19 +0200

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Денис Ільницький on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
